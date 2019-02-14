2c250ba31c2e2fad679874cad4f61c442597c185
shwang <wangshiheng8@gmail.com>
2019-02-14 17:20:29 +0800

    Update ssh_all.sh

--- script/ssh_all.sh
+++ script/ssh_all.sh
@@ -1,26 +1,22 @@
-#！ /bin/bash
-#做一个多级操作脚本，执行该脚本，来操作多台机器
+#!/bin/bash
 
-#获取当前脚本所在位置
-cd `dirname $0`
+#获取执行路径
+RUN_HOME=$(cd "$(dirname "$0")"; echo "${PWD}")
 
-#获取路径
-path=`pwd`
-
-#查看文件内容
-ip_arr=`cat "${path}/ips"`
+#查看hosts文件内容
+NOW_LIST=(`cat ${RUN_HOME}/ips`)
 
 #以数组的形式进行遍历
-for ip in ${ip_arr[*]}
-do
+SSH_USER="hadoop"
+for i in ${NOW_LIST[@]};do
     #拼接ssh命令，格式： ssh 用户名@主机名  操作
-    cmd="ssh luser@${ip} $*"
+    cmd="ssh $SSH_USER@$i \"$*\""
     echo $cmd
 
     #动态执行
     if eval $cmd ; then
-        echo "ok"
+        echo "OK"
     else
-        echo "fail"
+        echo "FAIL"
     fi
 done
